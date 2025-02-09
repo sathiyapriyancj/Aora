d4bfb9fca98e2ddb40a958a539ade4d596efb2ed
sathiyapriyancj <ssathiya.cs@gmail.com>
2025-02-09 17:13:00 +0530

    Custom Design Tailwind

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container} className="">
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <Link href="/profile">Go to profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

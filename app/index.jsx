9498f38ee4ce169f7f83481e42fce35d95cecb42
sathiyapriyancj <ssathiya.cs@gmail.com>
2025-02-10 12:51:47 +0530

    File & Folder Structure

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
-export default function App() {
+export default function IndexScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <Link href="/profile">Go to profile</Link>
+      <Text className="text-3xl font-bold">Aora!</Text>
+      <Link href={"/tabs/home"} className="text-blue-500 text-lg mt-4">
+        Go to Home
+      </Link>
     </View>
   );
 }
